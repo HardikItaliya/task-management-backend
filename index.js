a56b41ea3f83ca8bcc6942aa42eecf47180ad6a3
hardik <i.hardik722@gmail.com>
2024-09-19 12:36:01 +0530

    deployement changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const taskRoutes = require('./routes/taskRoutes');
+const taskRoutes = require('./src/routes/taskRoutes');
 require('dotenv').config();
 
 const app = express();
